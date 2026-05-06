382d139c4f4d3d5f1343d19e2855188b79406c6e
jake-t-dev <krysis15@outlook.com>
2026-05-06 23:02:32 +0100

    listen for player enter world event

--- Bagnon/utility/playerInfo.lua
+++ Bagnon/utility/playerInfo.lua
@@ -12,6 +12,13 @@ Bagnon.PlayerInfo = PlayerInfo
 
 local CURRENT_PLAYER = UnitName('player')
 
+-- Update current player name on login/character swap
+local playerInfoFrame = CreateFrame('Frame')
+playerInfoFrame:RegisterEvent('PLAYER_ENTERING_WORLD')
+playerInfoFrame:SetScript('OnEvent', function()
+	CURRENT_PLAYER = UnitName('player')
+end)
+
 function PlayerInfo:IsCached(player)
 	return player ~= CURRENT_PLAYER
 end
